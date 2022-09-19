f1ea251b1ce4b49923cbd99391d6a6e502b1af47
Luis Prado <luisprado0095@gmail.com>
2022-09-19 00:23:41 -0300

    starting symfony :fire:

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,25 @@ class ComposerAutoloaderInit916c3171150795210eba8af797f829e1
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit916c3171150795210eba8af797f829e1::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire916c3171150795210eba8af797f829e1($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire916c3171150795210eba8af797f829e1($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
